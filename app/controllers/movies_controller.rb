7455cacb64c2f573ba159062c89f4b3b93692ff5
Helen Huang <hhuang@uccs.edu>
2018-02-17 09:13:31 +0000

    Submitting HW2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,34 +1,30 @@
 class MoviesController < ApplicationController
-  
   def movie_params
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
   
-  # def initialize
-  #   @all_ratings = Movie.all_ratings
-  #   @all_ratings = ['G','PG','PG-13','R']
-  #   super
-  # end
-
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+    # Renders app/views/movies/show.<extension> by default
   end
 
   def index 
-    #@movies = Movie.all 
+    # Initial session and parameters.
+    # Choosing a sort will be remembered even when clicking out of the link movie list. 
     @sort_by = params[:sort] || session[:sort]
     @ratings = params[:ratings] || session[:ratings]
-    #first launch, session sort is empty and param sort might be empty, and as you choose to sort, they get data and they will remember
+    
     
     if @ratings.nil?
       ratings = Movie.ratings
     else
+       # keys: 'G', 'PG', 'PG-13', 'R'
       ratings = @ratings.keys
-      # keys that we put in the Movie.rb with p, pg, pg-13...
+     
     end
-      
+    
+    # Order by ascending 
     if !@sort_by.nil?
       @movies = Movie.order("#{@sort_by} ASC").where(:rating => ratings)
     else
@@ -40,30 +36,27 @@ class MoviesController < ApplicationController
       all_ratings
     end
     
-    #This is where we are remembering it now:
+    # Current session's sort is being stored here. 
     session[:sort] = @sort_by
     session[:ratings] = @ratings
-    
+    # Check that the database is not empty
     if params[:sort].nil? && params[:ratings].nil? && (!session[:sort].nil? || !session[:ratings].nil?)
       flash.keep
       redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
     end
     
   end 
-#order is saying put it in this order, by ascending order
-#if statement is making sure that the database is not empty 
-
+  
   def new
-    # default: render 'new' template
+    # Default rendering of new template
   end
 
   def create
     @movie = Movie.create!(movie_params)
+      # Tracks particular information of params and sessions
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
-  #flash keeps track of particular information
-  #params, sessions not hashes
 
   def edit
     @movie = Movie.find params[:id]
@@ -82,12 +75,4 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-  
-  # def hilight(column)
-  #   if(session[:order].to_s == column)
-  #     return 'hilite'
-  #   else
-  #     return nil
-  #   end
-  # end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,3 @@
-#do the business logic; goes into database and finds movie, make new variable THE MODEL FOR MOVIE
 class Movie < ActiveRecord::Base
     def self.ratings
         ['G', 'PG', 'PG-13', 'R']
